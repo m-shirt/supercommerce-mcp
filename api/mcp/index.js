c536eb2d4e2262b5f1d3721fd0e50aa627378754
SHIRT <shirt@el-dokan.com>
2025-08-09 14:41:04 +0300

    add server sse

--- api/mcp/index.js
+++ api/mcp/index.js
@@ -8,7 +8,7 @@ export const config = {
 
 function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
 }
 
